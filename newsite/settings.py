c67d0c9c76733b2ccc4d012c559a2593a41c067a
sreyansb <43511601+sreyansb@users.noreply.github.com>
2020-06-27 11:48:39 +0530

    Update settings.py

--- newsite/settings.py
+++ newsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wki+wmeyd*rs1^4=e$4d%ov5rn!++n6d@n8sj%xko-*h!g_@g$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
